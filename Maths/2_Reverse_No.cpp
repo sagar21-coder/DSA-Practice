3ea9e471d2c6a63387aaf1b8724c5386a299bc82
Sagar Ahirrao <sagarahirrao111@gmail.com>
2026-08-25 15:07:38 +0530

    Counting Digit

---
+++ Maths/2_Reverse_No.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n = 12345;
+    int reverse = 0;
+
+    while(n > 0) {
+
+       int lastDigit = n % 10;
+
+       reverse = reverse * 10 + lastDigit;
+
+       n = n / 10;
+
+    }
+
+    cout << reverse;
+
+    return 0;
+}
