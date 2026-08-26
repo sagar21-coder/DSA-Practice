f3654693d6c61978e7137e1ed464a3404687bdf1
Sagar Ahirrao <sagarahirrao111@gmail.com>
2026-08-26 22:42:27 +0530

    Remove Outer Parenthesis

---
+++ Strings/Remove_Outer_Parenthisis.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string s = "(()())(())";
+    string ans = "";
+    int count = 0;
+    
+    for(int i=0; i < s.length(); i++) {
+
+        if(s[i] == '(') {
+
+            if(count > 0) {
+                ans += s[i];
+            }
+            count++;
+        }
+
+        else {
+            count--;
+
+            if(count > 0) {
+                ans += s[i];
+            }
+        }
+
+    }
+
+    cout << ans;
+
+    return 0;
+}
