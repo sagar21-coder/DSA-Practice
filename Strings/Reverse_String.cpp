a9edf14a60951c5d3b895bd47b53f3e22a44aced
Sagar Ahirrao <sagarahirrao111@gmail.com>
2026-08-27 17:05:19 +0530

    Reverse Words in String

--- Strings/Reverse_String.cpp
+++ Strings/Reverse_String.cpp
@@ -1,22 +1,57 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     string s = "Hello";
+
+//     int left = 0;
+//     int right = s.length() - 1;
+
+//     while(left < right) {
+
+//         swap(s[left], s[right]);
+
+//         left++;
+//         right--;
+//     }
+
+//     cout << s;
+
+//     return 0;
+// }
+
+
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+
+// int main() {
+
+//     string s = " hello";
+
+//     reverse(s.begin(), s.end());
+//     cout << s;
+
+
+//     return 0;
+// }
+
+
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
 
-    string s = "Hello";
+    string s = "hello";
+    string temp = "";
 
-    int left = 0;
-    int right = s.length() - 1;
-
-    while(left < right) {
-
-        swap(s[left], s[right]);
-
-        left++;
-        right--;
+    for(int i = s.length() - 1; i >=0; i--) {
+        temp = temp + s[i];
     }
-
-    cout << s;
+    
+     cout << temp;
 
     return 0;
 }
---
+++ Strings/Reverse_Words_in_String.cpp
@@ -0,0 +1,55 @@
+// #include <iostream>
+// #include <algorithm>
+// #include <vector>
+// #include <sstream>
+// using namespace std;
+
+// int main () {
+
+//     string s = "the sky is blue";
+
+//     stringstream ss(s);
+//     vector<string> words;
+
+//     string word;
+
+//     while(ss >> word) {
+//         words.push_back(word);
+//     }
+
+//     for(int i = words.size()-1; i>=0; i--) {
+//         cout << words[i] << " ";
+//     }
+
+//     return 0;
+// }
+
+
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main () {
+
+   string s = "the sky is blue";
+
+   reverse(s.begin(), s.end());
+
+   int start = 0;
+
+   for(int i = 0; i <= s.length(); i++) {
+
+    if(i == s.length() || s[i] == ' ') {
+
+        reverse(s.begin()+start, s.begin()+i);
+
+        start = i + 1;
+    }
+   }
+
+   cout << s;
+
+    return 0;
+}
