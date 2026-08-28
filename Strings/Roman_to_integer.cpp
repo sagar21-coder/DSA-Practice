69306c34fdf430c24ed92ce99999d9f0d4792fd6
Sagar Ahirrao <sagarahirrao111@gmail.com>
2026-08-28 19:25:48 +0530

    Roman to integer

---
+++ Strings/Roman_to_integer.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+    int num(char c) {
+
+        if(c == 'I') return 1;
+        if(c == 'V') return 5;
+        if(c == 'X') return 10;
+        if(c == 'L') return 50;
+        if(c == 'C') return 100;
+        if(c == 'D') return 500;
+        if(c == 'M') return 1000;
+        
+        return 0;
+    }
+
+    int RomanToInt(string s) {
+
+        int sum = 0;
+        int index = 0;
+
+        while(index < s.size()-1) {
+
+            if(num(s[index]) < num(s[index + 1]))
+            sum = sum - num(s[index]);
+            
+            else
+            sum = sum + num(s[index]);
+
+            index++;
+        }
+
+        sum = sum + num(s[index]);
+
+        return sum;
+    }
+
+    int main() {
+
+        string s = "MCMIV";
+
+        cout << RomanToInt(s);
+
+    return 0;
+}
